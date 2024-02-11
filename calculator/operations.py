d8338ead09a3dca726b85cd8d6aa030bc31e322b
NageshKanneganti <nk726@njit.edu>
2024-02-11 17:46:44 -0600

    updated operations w/ basic arithmetic functions: +, -, *, /

---
+++ calculator/operations.py
@@ -0,0 +1,19 @@
+"""
+Contains simple arithmetic operations: addition, subtraction, multiplication, and division
+"""
+
+def addition(a,b):
+    '''Basic addition: returns sum of a & b'''
+    return a + b
+
+def subtraction(a,b):
+    '''Basic subtraction: returns difference of a & b'''
+    return a - b
+
+def multiplication(a,b):
+    '''Basic multiplication: returns product of a & b'''
+    return a * b
+
+def division(a,b):
+    '''Basic division: returns quotient of a (dividend) & b (divisor)'''
+    return a / b
