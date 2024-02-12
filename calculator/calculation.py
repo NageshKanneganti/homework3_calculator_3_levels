cf03997e75304237f871e35b6d9935e71d2381af
NageshKanneganti <nk726@njit.edu>
2024-02-11 22:26:03 -0600

    updated calculation.py: Added Calculation class for handeling individual calculations

---
+++ calculator/calculation.py
@@ -0,0 +1,20 @@
+"""
+Defines a single calculation. Provides abstraction for handeling individual calculations in the Calculator class.
+"""
+from decimal import Decimal
+from typing import Callable
+
+class Calculation:
+    '''Defines a single calculation'''
+
+    def __init__(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]) -> None:
+        '''Constructor method with type hints'''
+        # Initilize operands
+        self.a = a
+        self.b = b 
+        # Inilitlize arithmetic operation & stores operation as a callable that takes in two decimals 
+        self.operation = operation 
+
+    def get_result(self):
+        # Call the stored operation and pass it the stored operands
+        return self.operation(self.a, self.b)
