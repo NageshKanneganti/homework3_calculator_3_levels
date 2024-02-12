0b846d75d5db71a680ffe818a3df29a67b4fc88f
NageshKanneganti <nk726@njit.edu>
2024-02-11 22:29:53 -0600

    updated operations.py: Added type hints & captured divide by zero exception

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,17 +1,21 @@
 '''Contains simple arithmetic operations: addition, subtraction, multiplication, and division'''
+from decimal import Decimal # Define operation functions with type hints
 
-def addition(a,b):
+def addition(a: Decimal, b: Decimal) -> Decimal:
     '''Basic addition: returns sum of a & b'''
     return a + b
 
-def subtraction(a,b):
+def subtraction(a: Decimal, b: Decimal) -> Decimal:
     '''Basic subtraction: returns difference of a & b'''
     return a - b
 
-def multiplication(a,b):
+def multiplication(a: Decimal, b: Decimal) -> Decimal:
     '''Basic multiplication: returns product of a & b'''
     return a * b
 
-def division(a,b):
-    '''Basic division: returns quotient of a (dividend) & b (divisor)'''
-    return a / b
+def division(a: Decimal, b: Decimal) -> Decimal:
+    '''Basic division: returns quotient of a (dividend) & b (divisor); catch divide by zero exception'''
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    else:
+        return a / b
