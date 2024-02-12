46683efa9083858cdd29fa961608b98be322009c
NageshKanneganti <nk726@njit.edu>
2024-02-12 09:43:06 -0600

    changed visibility of perform_calculation method to make it private

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -10,7 +10,7 @@ class Calculator:
     '''Serves as a core componet of a basic calculator system. Integrates components for performing arithmetic calculations.'''
 
     @staticmethod
-    def perform_calculation(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]) -> Decimal:
+    def _perform_calculation(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]) -> Decimal:
         '''Performs a operation with the given operands and operation, and returns the result'''
         calculation = Calculation(a, b, operation)
         return calculation.compute()
