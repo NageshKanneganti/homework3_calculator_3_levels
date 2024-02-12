f5f4afb4f51f870877e952e83dda4806d738eff3
NageshKanneganti <nk726@njit.edu>
2024-02-12 11:25:15 -0600

    added @staticmethod tags to Operations class methods

--- calculator/operations.py
+++ calculator/operations.py
@@ -2,18 +2,23 @@
 from decimal import Decimal # Define operation functions with type hints
 
 class Operations():
+    '''Basic arithemtic operations for calculator'''
+    
+    @staticmethod
     def addition(a: Decimal, b: Decimal) -> Decimal:
         '''Basic addition: returns sum of a & b'''
         return a + b
-
+    @staticmethod
     def subtraction(a: Decimal, b: Decimal) -> Decimal:
         '''Basic subtraction: returns difference of a & b'''
         return a - b
-
+    
+    @staticmethod
     def multiplication(a: Decimal, b: Decimal) -> Decimal:
         '''Basic multiplication: returns product of a & b'''
         return a * b
 
+    @staticmethod
     def division(a: Decimal, b: Decimal) -> Decimal:
         '''Basic division: returns quotient of a (dividend) & b (divisor); catch divide by zero exception'''
         if b == 0:
