072578892e709967c244c7a97249fe49fe74daa2
NageshKanneganti <nk726@njit.edu>
2024-02-13 16:38:37 -0600

    updated operations test file with parametrized test data

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,28 +1,24 @@
 '''Tests operations.py'''
+# Disable specific pylint warnings that are not relevant for this file.
+# pylint: disable=unnecessary-dunder-call, invalid-name
 from decimal import Decimal
 import pytest
 from calculator.operations import Operations as op
 
-# Test cases for operations.py"
-def test_operation_addition():
-    '''Tests that addition function works'''
-    assert op.addition(Decimal('2'), Decimal('3')) == Decimal('5')
-    assert op.addition(Decimal('0.1'), Decimal('0.2')) == Decimal('0.3')
+@pytest.mark.parametrize("operation, a, b, expected", [
+    (op.addition, Decimal('2'), Decimal('3'), Decimal('5')),
+    (op.addition, Decimal('0.1'), Decimal('0.2'), Decimal('0.3')),
+    (op.subtraction, Decimal('5'), Decimal('3'), Decimal('2')),
+    (op.subtraction, Decimal('0.3'), Decimal('0.1'), Decimal('0.2')),
+    (op.multiplication, Decimal('2'), Decimal('3'), Decimal('6')),
+    (op.multiplication, Decimal('0.1'), Decimal('0.2'), Decimal('0.02')),
+    (op.division, Decimal('6'), Decimal('3'), Decimal('2')),
+    (op.division, Decimal('0.3'), Decimal('0.1'), Decimal('3'))
+])
 
-def test_operation_subtraction():
-    '''Tests that subtraction function works'''
-    assert op.subtraction(Decimal('5'), Decimal('3')) == Decimal('2')
-    assert op.subtraction(Decimal('0.3'), Decimal('0.1')) == Decimal('0.2')
-
-def test_operation_multiplication():
-    '''Tests that multiplication function works'''
-    assert op.multiplication(Decimal('2'), Decimal('3')) == Decimal('6')
-    assert op.multiplication(Decimal('0.1'), Decimal('0.2')) == Decimal('0.02')
-
-def test_operation_division():
-    '''Tests that division function works'''
-    assert op.division(Decimal('6'), Decimal('3')) == Decimal('2')
-    assert op.division(Decimal('0.3'), Decimal('0.1')) == Decimal('3')
+def test_operations(operation, a, b, expected):
+    '''Tests various operations with parametrized data'''
+    assert operation(a, b) == expected
 
 def test_operation_divide_by_zero():
     '''Test case for division by zero'''
