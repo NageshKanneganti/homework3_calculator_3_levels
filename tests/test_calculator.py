4b3f00c48ef1a9484a843cf87224dd1d6fb73a19
NageshKanneganti <nk726@njit.edu>
2024-02-13 16:51:56 -0600

    updated calculator class test file w/ parametrized test data

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,24 +1,22 @@
 '''Tests Calculator class from __init__.py'''
+# Disable specific pylint warnings that are not relevant for this file.
+# pylint: disable=unnecessary-dunder-call, invalid-name
 import pytest
 from calculator import Calculator
 
-def test_calculator_addition():
-    '''Test that addition function works '''    
-    assert Calculator.add(2,2) == 4
-
-def test_calculator_subtraction():
-    '''Test that addition function works '''    
-    assert Calculator.subtract(2,2) == 0
-
-def test_calculator_multiplication():
-    '''Test that division function works '''    
-    assert Calculator.multiply(2,2) == 4
-
-def test_calculator_division():
-    '''Test that multiply function works '''    
-    assert Calculator.divide(2,2) == 1
+@pytest.mark.parametrize("calculator_op, a, b, expected", [
+    (Calculator.add, 2, 2, 4),
+    (Calculator.subtract, 2, 2, 0),
+    (Calculator.multiply, 2, 2, 4),
+    (Calculator.divide, 2, 2, 1),
+    (Calculator.divide, 6, 2, 3),
+    (Calculator.divide, -1, -1, 1)
+])
+def test_calculator_operations(calculator_op, a, b, expected):
+    '''Test Calculator class operations'''
+    assert calculator_op(a, b) == expected, f"Failed {calculator_op.__name__} operation with {a} and {b}"
 
 def test_calculator_divide_by_zero():
     '''Test calculator divide by zero exception'''
     with pytest.raises(ValueError):
-        Calculator.divide(2,0)
+        Calculator.divide(2, 0)
