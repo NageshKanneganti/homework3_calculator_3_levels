158172070ea8e6057c440def03df63ace5e11493
NageshKanneganti <nk726@njit.edu>
2024-02-14 08:33:26 -0600

    updated test_calculator.py parameterized test data with Decimal type hints

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,17 +1,21 @@
 '''Tests Calculator class from __init__.py'''
 # Disable specific pylint warnings that are not relevant for this file.
 # pylint: disable=unnecessary-dunder-call, invalid-name
+from decimal import Decimal
 import pytest
 from calculator import Calculator
 
 @pytest.mark.parametrize("calculator_op, a, b, expected", [
-    (Calculator.add, 2, 2, 4),
-    (Calculator.subtract, 2, 2, 0),
-    (Calculator.multiply, 2, 2, 4),
-    (Calculator.divide, 2, 2, 1),
-    (Calculator.divide, 6, 2, 3),
-    (Calculator.divide, -1, -1, 1)
+    (Calculator.add, Decimal('2'), Decimal('2'), Decimal('4')),
+    (Calculator.subtract, Decimal('2'), Decimal('2'), Decimal('0')),
+    (Calculator.multiply, Decimal('2'), Decimal('2'), Decimal('4')),
+    (Calculator.divide, Decimal('2'), Decimal('2'), Decimal('1')),
+    (Calculator.add, Decimal('1.0'), Decimal('2.0'), Decimal('3.0')),
+    (Calculator.subtract, Decimal('3.0'), Decimal('2.0'), Decimal('1.0')),
+    (Calculator.multiply, Decimal('2.0'), Decimal('3.0'), Decimal('6.0')),
+    (Calculator.divide, Decimal('6.0'), Decimal('2.0'), Decimal('3.0')),
 ])
+
 def test_calculator_operations(calculator_op, a, b, expected):
     '''Test Calculator class operations'''
     assert calculator_op(a, b) == expected, f"Failed {calculator_op.__name__} operation with {a} and {b}"
